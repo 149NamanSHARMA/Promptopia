1baa01088343eaa9d46cdc072d1052803fe8f07f
149NamanSHARMA <103133304+149NamanSHARMA@users.noreply.github.com>
2024-08-17 16:54:57 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
+// Use ES Modules export syntax
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ]
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config;
+  }
+};
 
+// Corrected export statement for ES Modules
 export default nextConfig;
